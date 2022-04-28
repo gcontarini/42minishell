97c8766853f90e86967daae2de994b52ea332652
nprimo <primo.niccolo@gmail.com>
2022-04-28 16:55:27 +0100

    Update parser.h with new command_list struct

--- inc/parser.h
+++ inc/parser.h
@@ -6,7 +6,7 @@
 /*   By: nprimo <nprimo@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/22 16:24:43 by nprimo            #+#    #+#             */
-/*   Updated: 2022/04/28 16:26:13 by nprimo           ###   ########.fr       */
+/*   Updated: 2022/04/28 16:47:26 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,21 @@
 # define METACHAR_SET "|&;()<>"
 # define SPACE_SET " \n\t"
 
+typedef struct s_fd
+{
+	char	*fname;
+	int		fd;
+}	t_fd;
+
 typedef struct s_comm_list
 {
 	char				**av;
-	int					input;
-	int					output;
+	t_fd				in;
+	t_fd				out;
 	struct s_comm_list	*next;
 }	t_comm_list;
 
-void	get_token_list(char *input, t_list **token_list);
+void		get_token_list(char *input, t_list **token_list);
+t_comm_list	*get_comm_list(t_list *token_list);
 
 #endif
