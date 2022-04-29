3ae41c8aef711ca189c264ac91e803a9b69aebbc
nprimo <primo.niccolo@gmail.com>
2022-04-29 14:58:49 +0100

    Update get_comm_list
    Add free for token list

--- src/parser/get_comm_list.c
+++ src/parser/get_comm_list.c
@@ -6,7 +6,7 @@
 /*   By: nprimo <nprimo@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/28 16:59:38 by nprimo            #+#    #+#             */
-/*   Updated: 2022/04/29 12:27:57 by nprimo           ###   ########.fr       */
+/*   Updated: 2022/04/29 14:54:07 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ t_cmd	*get_comm_list(char *input)
 {
 	t_cmd	*comm_list; // need to be dynamic allocation
 	t_list	*token_list;
+	t_list	*head;
 	int		pos;
 
 	token_list = NULL;
@@ -25,12 +26,14 @@ t_cmd	*get_comm_list(char *input)
 	comm_list = malloc(sizeof(t_cmd) * 2);
 	comm_list[0].av = malloc(sizeof(comm_list[0].av) * (ft_lstsize(token_list) + 1));
 	pos = 0;
-	while (token_list)
+	head = token_list;
+	while (head)
 	{
-		comm_list[0].av[pos] = token_list->content;
-		token_list = token_list->next;
+		comm_list[0].av[pos] = head->content;
+		head = head->next;
 		pos++;
 	}
+	ft_lstclear(&token_list, free);
 	comm_list[0].av[pos] = NULL;
 	comm_list[0].in.fname = NULL;
 	comm_list[0].out.fname = NULL;
