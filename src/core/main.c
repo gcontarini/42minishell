3dbe5a6811aa081ab630ea81a5f84106492e5cee
nprimo <primo.niccolo@gmail.com>
2022-04-29 14:48:42 +0100

    Update main.c
    Remove while loop - causes segfault

--- src/core/main.c
+++ src/core/main.c
@@ -6,7 +6,7 @@
 /*   By: nprimo <nprimo@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 11:14:41 by gcontarini        #+#    #+#             */
-/*   Updated: 2022/04/28 18:07:41 by nprimo           ###   ########.fr       */
+/*   Updated: 2022/04/29 14:48:28 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,15 @@ int main(void)
 
     //sh = init_sh();
     //register_all_signals(); // Add functions to handle each signal
-    while (true)
-    {
-        sh.inpt = readline(PROMPT);
-        add_history(sh.inpt);
-        ft_printf("INPUT: %s\n", sh.inpt);
-        comm_list = get_comm_list(sh.inpt);
-        if (exec_comm_list(comm_list) == 1)
-            break ;
-        //exec_inpt(parse_input() => do_cmd()); // => wait for process???
-        //free_input(); Check if we can free memory that is added to history
-    }
+    // find a way to loop: while loop cause segfault
+    sh.inpt = readline(PROMPT);
+    add_history(sh.inpt);
+    ft_printf("INPUT: %s\n", sh.inpt);
+    comm_list = get_comm_list(sh.inpt);
+    if (exec_comm_list(comm_list) == 1)
+        return (1) ;
+    //exec_inpt(parse_input() => do_cmd()); // => wait for process???
+    //free_input(); Check if we can free memory that is added to history
     //free_and_exit(); // Exit gracefully
     return (0);
 } 
