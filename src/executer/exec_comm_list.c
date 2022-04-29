cb3a8babf9e09c661c153fc90299b07ba6af50dd
nprimo <primo.niccolo@gmail.com>
2022-04-29 12:29:47 +0100

    Upate exec_comm_list

--- src/executer/exec_comm_list.c
+++ src/executer/exec_comm_list.c
@@ -6,7 +6,7 @@
 /*   By: nprimo <nprimo@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/28 17:19:09 by nprimo            #+#    #+#             */
-/*   Updated: 2022/04/28 17:45:47 by nprimo           ###   ########.fr       */
+/*   Updated: 2022/04/29 12:16:52 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,11 @@ int exec_comm_list(t_cmd *comm_list)
 		// 	comm_list->in.fd = open(comm_list->in.fname, O_RDONLY);
 		// 	...
 		// }
-		if (!comm_list->out.fname)
-			comm_list->out.fd = 2;
+		if (!comm_list[pos].out.fname)
+			comm_list[pos].out.fd = 2;
 		// same as for input
-		if (exec_command(comm_list->in.fd, comm_list->out.fd, comm_list->av, NULL))
+		if (exec_command(comm_list[pos].in.fd, comm_list[pos].out.fd, 
+				comm_list[pos].av, NULL))
 			return (1);
 		pos++;
 	}
