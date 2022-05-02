c99007bbba400e79c2b8a57028f25cef44e84410
nprimo <primo.niccolo@gmail.com>
2022-05-02 10:28:40 +0100

    Update get_cmd_list
    Comment ft_lstclear for token_list; generating errors

--- src/parser/get_cmd_list.c
+++ src/parser/get_cmd_list.c
@@ -6,7 +6,7 @@
 /*   By: nprimo <nprimo@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/28 16:59:38 by nprimo            #+#    #+#             */
-/*   Updated: 2022/05/02 10:15:23 by nprimo           ###   ########.fr       */
+/*   Updated: 2022/05/02 10:27:56 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ t_list	*get_cmd_list(char *input)
 	head = token_list;
 	while (head)
 		head = add_new_cmd(&cmd_list, head);
-	ft_lstclear(&token_list, NULL);
+	// ft_lstclear(&token_list, NULL);
 	return (cmd_list);
 }
 
