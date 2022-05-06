9374cccd9d270d3cd3f8adf9801379d0acd5ce8c
nprimo <primo.niccolo@gmail.com>
2022-05-06 10:49:01 +0100

    Update find_bin_path
    Add expand_cmd to handle cases where av[0] is relative or
    absolute path

--- src/executer/find_bin_path.c
+++ src/executer/find_bin_path.c
@@ -6,13 +6,14 @@
 /*   By: nprimo <nprimo@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/29 12:21:53 by nprimo            #+#    #+#             */
-/*   Updated: 2022/05/06 10:37:03 by nprimo           ###   ########.fr       */
+/*   Updated: 2022/05/06 10:48:18 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 static char	**find_ls_path_dir(void);
+static char	*expand_cmd(const char *cmd);
 
 char	*find_bin_path(const char *cmd)
 {
@@ -23,16 +24,15 @@ char	*find_bin_path(const char *cmd)
 
 	pos = 0;
 	cmd_path = NULL;
-	if (access(cmd, X_OK))
-		return (cmd); // might need to be expanded
-	ls_path_dir = find_ls_path_dir();
-	if (!ls_path_dir)
+	if (access(cmd, F_OK & X_OK) == 0)
+	{
+		cmd_path = expand_cmd(cmd);
 		return (cmd_path);
+	}
+	ls_path_dir = error_check_pointer(find_ls_path_dir());
 	while (ls_path_dir[pos])
 	{
-		dir = ft_strjoin(ls_path_dir[pos], "/");
-		if (dir == NULL)
-			break ;
+		dir = error_check_pointer(ft_strjoin(ls_path_dir[pos], "/"));
 		cmd_path = ft_strjoin(dir, cmd);
 		free(dir);
 		if (cmd_path == NULL || access(cmd_path, X_OK) == 0)
@@ -52,8 +52,15 @@ static char	**find_ls_path_dir(void)
 	path_var = getenv("PATH");
 	if (!path_var)
 		return (NULL);
-	ls_path_dir = ft_split(path_var, ':');
-	if (!ls_path_dir)
-		ls_path_dir = NULL;
+	ls_path_dir = error_check_pointer(ft_split(path_var, ':'));
 	return (ls_path_dir);
+}
+
+static char *expand_cmd(const char *cmd)
+{
+	char *cmd_path;
+
+	cmd_path = error_check_pointer(ft_strdup(cmd));
+	// or expand ../ or ./ if needed;
+	return (cmd_path);
 }
