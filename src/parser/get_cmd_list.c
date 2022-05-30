fa16aca83283cff5d765ca2b47f31c7d56acba10
nprimo <primo.niccolo@gmail.com>
2022-05-30 16:50:16 +0100

    Update get_cmd_list.c
    Add add_cmd_token_list function

--- src/parser/get_cmd_list.c
+++ src/parser/get_cmd_list.c
@@ -6,7 +6,7 @@
 /*   By: nprimo <nprimo@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/28 16:59:38 by nprimo            #+#    #+#             */
-/*   Updated: 2022/05/30 16:28:21 by nprimo           ###   ########.fr       */
+/*   Updated: 2022/05/30 16:45:34 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 static char		**llist_n_to_av(t_list *llist, int ac);
 static t_list	*add_new_cmd(t_list **cmd_list, t_list *token_list);
 static t_cmd	*init_new_cmd(void);
+static t_list	*add_cmd_token_list(t_list *token_list, int argc);
 
 t_list	*get_cmd_list(t_list *token_list)
 {
@@ -41,23 +42,36 @@ static t_list	*add_new_cmd(t_list **cmd_list, t_list *token_list)
 	new_cmd = init_new_cmd();
 	argc = 0;
 	head = token_list;
-	if (is_control_operator(head->content))
-		argc++;
 	while (head)
 	{
 		if (is_control_operator(head->content))
 		{
 			argc++;
+			head = head->next;
 			break ;
 		}
 		argc++;
 		head = head->next;
 	}
-	new_cmd->av = (char **)error_check_pointer(llist_n_to_av(token_list, argc));
-	// update cmd t_fd according to arguments
+	new_cmd->token_list = add_cmd_token_list(token_list, argc);
 	new_node = (t_list *)error_check_pointer(ft_lstnew(new_cmd));
 	ft_lstadd_back(cmd_list, new_node);
 	return (head);
+}
+
+static t_list	*add_cmd_token_list(t_list *token_list, int argc)
+{
+	t_list	*head;
+
+	head = token_list;
+	while (head && argc > 1)
+	{
+		argc--;
+		head = head->next;
+	}
+	if (head)
+		head->next = NULL;
+	return (token_list);
 }
 
 static char	**llist_n_to_av(t_list *llist, int argc)
