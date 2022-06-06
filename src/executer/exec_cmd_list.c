6d2d061f89569a0b842975698fdbe99bf9ad57b9
nprimo <primo.niccolo@gmail.com>
2022-06-06 17:36:04 +0100

    Update exec_cmd_list.c

--- src/executer/exec_cmd_list.c
+++ src/executer/exec_cmd_list.c
@@ -6,63 +6,24 @@
 /*   By: nprimo <nprimo@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/28 17:19:09 by nprimo            #+#    #+#             */
-/*   Updated: 2022/05/02 12:21:22 by nprimo           ###   ########.fr       */
+/*   Updated: 2022/06/06 17:33:57 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int	get_fd_in(t_cmd	cmd);
-static int	get_fd_out(t_cmd cmd);
-
-int exec_cmd_list(t_list *cmd_list)
+int	exec_cmd_list(t_list *cmd_list)
 {
-	t_list  *head;
+	t_list	*head;
 	t_cmd	*cmd;
 
 	head = cmd_list;
 	while (head)
 	{
-		cmd = (t_cmd *) (head->content);
-		cmd->in.fd = get_fd_in(*cmd);
-		cmd->out.fd = get_fd_out(*cmd);
-		if (cmd->out.fd == - 1 || cmd->in.fd == -1)
-			return (1);
+		cmd = (t_cmd *) head->content;
 		if (exec_cmd(*cmd, NULL))
 			return (1);
 		head = head->next;
 	}
 	return (0);
-}
-
-static int	get_fd_in(t_cmd cmd)
-{
-	int	fd;
-
-	if (!cmd.in.fname)
-			fd = STDIN_FILENO;
-	// else if for PIPE ?
-	else
-	{
-		fd = open(cmd.in.fname, O_RDONLY);
-		if (fd == -1)
-			return (-1);
-	}
-	return (fd);
-}
-
-static int	get_fd_out(t_cmd cmd)
-{
-	int	fd;
-
-	if (!cmd.in.fname)
-			fd = STDOUT_FILENO;
-	// else if for PIPE ?
-	else
-	{
-		fd = open(cmd.in.fname, O_WRONLY | O_CREAT);
-		if (fd == -1)
-			return (-1);
-	}
-	return (fd);
 }
