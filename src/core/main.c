149f72250c9658e83744d77b1b43f5db4027fe05
nprimo <primo.niccolo@gmail.com>
2022-06-13 17:48:53 +0100

    Update main.c
    Add while loop to test multiple commands in sequence

--- src/core/main.c
+++ src/core/main.c
@@ -6,7 +6,7 @@
 /*   By: nprimo <nprimo@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 11:14:41 by gcontarini        #+#    #+#             */
-/*   Updated: 2022/06/13 15:29:54 by nprimo           ###   ########.fr       */
+/*   Updated: 2022/06/13 17:48:00 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,17 @@ int main(void)
     //sh = init_sh();
     //register_all_signals(); // Add functions to handle each signal
     // find a way to loop: while loop cause segfault
-    sh.inpt = readline(PROMPT);
-    add_history(sh.inpt);
-    ft_printf("INPUT: %s\n", sh.inpt);
-    token_list = NULL;
-    error_check(get_token_list(sh.inpt, &token_list));
-    free(sh.inpt);
-    cmd_list = get_cmd_list(token_list);
-    open_fd(cmd_list);
-    print_cmd_list(cmd_list);
-    exec_cmd_list(cmd_list);
-    free_cmd_list(&cmd_list);
+    while (1)
+    {
+        sh.inpt = readline(PROMPT);
+        add_history(sh.inpt);
+        token_list = NULL;
+        error_check(get_token_list(sh.inpt, &token_list));
+        free(sh.inpt);
+        cmd_list = get_cmd_list(token_list);
+        open_fd(cmd_list);
+        exec_cmd_list(cmd_list);
+        free_cmd_list(&cmd_list);
+    }
     return (0);
 } 
