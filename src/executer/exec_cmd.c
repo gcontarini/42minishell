e7db587f7c282bb41dad06f9705de657419c9193
nprimo <primo.niccolo@gmail.com>
2022-06-20 12:40:40 +0100

    Update exec_cmd.c
    Add exec_bin

--- src/executer/exec_cmd.c
+++ src/executer/exec_cmd.c
@@ -6,15 +6,60 @@
 /*   By: nprimo <nprimo@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 12:24:52 by nprimo            #+#    #+#             */
-/*   Updated: 2022/06/20 11:55:40 by nprimo           ###   ########.fr       */
+/*   Updated: 2022/06/20 12:39:46 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static int	is_builitin(char *str);
 static int	redirect(int fd_in, int fd_out);
+static int	exec_bin(t_cmd *cmd, t_shell sh);
+static int	exec_builtin(t_cmd *cmd, t_shell sh);
 
 int	exec_cmd(t_cmd *cmd, t_shell sh)
+{
+	if (is_builitin(cmd->av[0]))
+		return (exec_builtin(cmd, sh));
+	else
+		return (exec_bin(cmd, sh));
+}
+
+static int	is_builitin(char *str)
+{
+	static char	*builtin_list[] = {
+		"echo", "env", NULL};
+	int			pos;
+
+	pos = 0;
+	while (builtin_list[pos])
+	{
+		if (ft_strncmp(builtin_list[pos], str,
+				ft_strlen(builtin_list[pos]) + 1) == 0)
+			return (1);
+		pos++;
+	}
+	return (0);
+}
+
+static int	redirect(int fd_in, int fd_out)
+{
+	if (fd_in != STDIN_FILENO)
+	{
+		if (dup2(fd_in, STDIN_FILENO) == -1
+			|| close(fd_in) == -1)
+			return (-1);
+	}
+	if (fd_out != STDOUT_FILENO)
+	{
+		if (dup2(fd_out, STDOUT_FILENO) == -1
+			|| close(fd_out) == -1)
+			return (-1);
+	}
+	return (0);
+}
+
+static int	exec_bin(t_cmd *cmd, t_shell sh)
 {
 	pid_t	pid;
 	char	*bin_path;
@@ -38,19 +83,18 @@ int	exec_cmd(t_cmd *cmd, t_shell sh)
 	return (0);
 }
 
-static int	redirect(int fd_in, int fd_out)
+int	exec_builtin(t_cmd *cmd, t_shell sh)
 {
-	if (fd_in != STDIN_FILENO)
-	{
-		if (dup2(fd_in, STDIN_FILENO) == -1
-			|| close(fd_in) == -1)
-			return (-1);
-	}
-	if (fd_out != STDOUT_FILENO)
-	{
-		if (dup2(fd_out, STDOUT_FILENO) == -1
-			|| close(fd_out) == -1)
-			return (-1);
-	}
+	int	return_status;
+
+	return_status = 0;
+	if (ft_strncmp("echo", cmd->av[0], ft_strlen("echo") + 1) == 0)
+		return_status = ft_echo(cmd);
+	else if (ft_strncmp("env", cmd->av[0], ft_strlen("env") + 1) == 0)
+		return_status = ft_env(cmd, sh);
+	if (cmd->in.fd != STDIN_FILENO)
+		close(cmd->in.fd);
+	if (cmd->out.fd != STDOUT_FILENO)
+		close(cmd->out.fd);
 	return (0);
 }
