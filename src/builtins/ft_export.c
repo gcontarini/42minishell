221bf6deaadce0dfeffbb42d18e86a8709f18999
nprimo <primo.niccolo@gmail.com>
2022-06-20 18:50:05 +0100

    Update ft_export.c
    Add get_dict_var function to be able to add new variables or
    update values of old ones

--- src/builtins/ft_export.c
+++ src/builtins/ft_export.c
@@ -6,7 +6,7 @@
 /*   By: nprimo <nprimo@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 16:53:26 by nprimo            #+#    #+#             */
-/*   Updated: 2022/06/20 17:12:10 by nprimo           ###   ########.fr       */
+/*   Updated: 2022/06/20 18:48:58 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,29 @@
 
 static int	get_ac(char **av);
 static void	display_env(t_list *env, int fd);
+static int	add_to_env(char *key_value, t_shell sh);
 
 int	ft_export(t_cmd *cmd, t_shell sh)
 {
+	int	pos;
+
 	if (get_ac(cmd->av) == 1)
 		display_env(sh.env, cmd->out.fd);
+	else
+	{
+		pos = 1;
+		while (cmd->av[pos])
+		{
+			add_to_env(cmd->av[pos], sh);
+			pos++;
+		}
+	}
 	return (0);
 }
 
 static int	get_ac(char **av)
 {
-	int pos;
+	int	pos;
 
 	pos = 0;
 	while (av[pos])
@@ -50,4 +62,30 @@ static void	display_env(t_list *env, int fd)
 		write(fd, "\n", 1);
 		env = env->next;
 	}
+}
+
+static int	add_to_env(char *key_value, t_shell sh)
+{
+	t_dict	*var;
+	t_dict	*new_var;
+	t_list	*new_el;
+	char	*tmp;
+
+	new_var = str_to_dict(key_value);
+	if (get_dict_var(new_var->key, sh.env))
+	{
+		var = get_dict_var(new_var->key, sh.env);
+		if (new_var->value)
+		{
+			tmp = var->value;
+			var->value = new_var->value;
+			free(tmp);
+		}
+	}
+	else
+	{
+		new_el = error_check_pointer(ft_lstnew(new_var));
+		ft_lstadd_back(&sh.env, new_el);
+	}
+	return (0);
 }
---
+++ src/core/get_dict_var.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_dict_var.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nprimo <nprimo@student.42lisboa.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/20 18:10:37 by nprimo            #+#    #+#             */
+/*   Updated: 2022/06/20 18:13:00 by nprimo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+t_dict	*get_dict_var(const char *key, t_list *dict)
+{
+	t_dict	*var;
+
+	while (dict)
+	{
+		var = (t_dict *) dict->content;
+		if (ft_strncmp(key, var->key, ft_strlen(key) + 1) == 0)
+			return (var);
+		dict = dict->next;
+	}
+	return (NULL);
+}
