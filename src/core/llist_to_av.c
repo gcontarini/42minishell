44ce795bf98906fb6377cd76d12e30a083e39da9
nprimo <primo.niccolo@gmail.com>
2022-06-25 15:41:47 +0100

    Update llist_to_av.c
    Fix norminette

--- src/core/llist_to_av.c
+++ src/core/llist_to_av.c
@@ -6,7 +6,7 @@
 /*   By: nprimo <nprimo@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 17:02:05 by nprimo            #+#    #+#             */
-/*   Updated: 2022/05/30 17:04:58 by nprimo           ###   ########.fr       */
+/*   Updated: 2022/06/25 15:41:30 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ char	**llist_to_av(t_list *llist)
 	t_list	*head;
 	int		pos;
 
-	av = (char **)error_check_pointer(malloc(sizeof(char *) 
-		* (ft_lstsize(llist) + 1)));
+	av = (char **)error_check_pointer(malloc(sizeof(char *)
+				* (ft_lstsize(llist) + 1)));
 	head = llist;
 	pos = 0;
 	while (head)
