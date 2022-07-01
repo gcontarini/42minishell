3b5503998947d41f79100b72903431285819de51
nprimo <primo.niccolo@gmail.com>
2022-07-01 15:38:46 +0100

    Add here_doc.c

---
+++ src/executer/here_doc.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   here_doc.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nprimo <nprimo@student.42lisboa.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/01 15:36:50 by nprimo            #+#    #+#             */
+/*   Updated: 2022/07/01 15:40:40 by nprimo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	here_doc(char *eof)
+{
+	int		fd_pipe[2];
+	char	*new_line;
+
+	if (pipe(fd_pipe) == -1)
+		return (-1);
+	new_line = readline("> ");
+	while (ft_strncmp(new_line, eof, ft_strlen(eof) + 1))
+	{
+		ft_putstr_fd(new_line, fd_pipe[1]);
+		free(new_line);
+		ft_putstr_fd("\n", fd_pipe[1]);
+		new_line = readline("> ");
+	}
+	free(new_line);
+	return (fd_pipe[0]);
+}
