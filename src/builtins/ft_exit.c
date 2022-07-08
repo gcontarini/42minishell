db7a8cfeb8a937215becc53f5db3684beb47163a
nprimo <primo.niccolo@gmail.com>
2022-07-08 13:23:31 +0200

    Update exit builtin to deal with arguments

--- src/builtins/ft_exit.c
+++ src/builtins/ft_exit.c
@@ -6,15 +6,53 @@
 /*   By: nprimo <nprimo@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 19:11:28 by nprimo            #+#    #+#             */
-/*   Updated: 2022/06/20 19:21:38 by nprimo           ###   ########.fr       */
+/*   Updated: 2022/07/08 13:22:13 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+#define ERORR_AC "bash: exit: too many arguments\n"
+
+int			get_ac(char **av);
+static int	is_all_digit(char *str);
+
 int	ft_exit(t_cmd *cmd, t_shell sh)
 {
 	cmd = (void *) cmd;
+	if (get_ac(cmd->av) >= 2)
+	{
+		if (is_all_digit(cmd->av[1]))
+		{
+			if (get_ac(cmd->av) > 2)
+			{
+				ft_putstr_fd(ERORR_AC, 2);
+				return (1);
+			}
+			sh.exit_status = ft_atoi(cmd->av[1]);
+			free_shell(sh);
+			exit(sh.exit_status);
+		}
+		else
+		{
+			ft_putstr_fd("bash:exit: ", 2);
+			ft_putstr_fd(cmd->av[1], 2);
+			ft_putstr_fd(": numeric argument required\n", 2);
+			free_shell(sh);
+			exit(2);
+		}
+	}
 	free_shell(sh);
-	exit(0);
+	exit(sh.exit_status);
+}
+
+static int	is_all_digit(char *str)
+{
+	while (str && *str)
+	{
+		if (!ft_isdigit(*str))
+			return (0);
+		str++;
+	}
+	return (1);
 }
