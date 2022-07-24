9c50c32fc62368b856e8323933855ec848cf3b9b
Gabriel Contarini <gcontarini@gmail.com>
2022-07-24 16:12:26 +0100

    Change terminal config to change only if option is set

--- src/terminal/ms_config_termios.c
+++ src/terminal/ms_config_termios.c
@@ -6,7 +6,7 @@
 /*   By: gcontari <gcontari@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/21 18:45:51 by gcontari          #+#    #+#             */
-/*   Updated: 2022/07/21 19:40:27 by gcontari         ###   ########.fr       */
+/*   Updated: 2022/07/24 15:58:22 by gcontari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ t_term	ms_config_termios(t_shell *sh)
 	t_term	new_term;
 
 	new_term = sh->old_term;
-	new_term.c_lflag &= ~ECHOCTL;
+	if ((ECHOCTL & new_term.c_lflag) == ECHOCTL)
+		new_term.c_lflag &= ~ECHOCTL;
 	return (new_term);
 }
