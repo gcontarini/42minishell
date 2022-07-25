951a43fd470b09531b8d8550c5ec090741563143
Gabriel Contarini <gcontarini@gmail.com>
2022-07-25 18:28:07 +0100

    Change stuff

--- src/core/shell.c
+++ src/core/shell.c
@@ -6,7 +6,7 @@
 /*   By: gcontari <gcontari@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 16:24:04 by nprimo            #+#    #+#             */
-/*   Updated: 2022/07/25 17:53:22 by gcontari         ###   ########.fr       */
+/*   Updated: 2022/07/25 18:27:41 by gcontari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int	shell_interactive(t_shell sh)
 		{
 			ms_restore_term(&sh);
 			free_shell(sh);
-			write(2, "exit\n", 5);
+			write(STDERR_FILENO, "exit\n", 5);
 			exit(0);
 		}
 		else if (ft_strlen(sh.input) > 0)
