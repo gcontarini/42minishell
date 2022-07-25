52e0905122b7218f445ae223c371b70e3ebad3ac
Gabriel Contarini <gcontarini@gmail.com>
2022-07-25 19:06:15 +0100

    Add printf

--- src/executer/here_doc.c
+++ src/executer/here_doc.c
@@ -6,7 +6,7 @@
 /*   By: gcontari <gcontari@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 15:36:50 by nprimo            #+#    #+#             */
-/*   Updated: 2022/07/25 18:22:10 by gcontari         ###   ########.fr       */
+/*   Updated: 2022/07/25 19:06:02 by gcontari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,7 @@ int	here_doc(char *eof, t_shell *sh)
 		return (0);
 	}
 	waitpid(0, &status, 0);
+	printf("STATUS: %d\n", status);
 	if (status != 0)
 		return (_close_pipe(fd_pipe, -1)); // error no?
 	return (fd_pipe[0]);
