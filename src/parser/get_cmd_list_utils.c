a9344c0e9efb1f19b3da786cbb2cece969f73a85
Niccolo Primo <nprimo@c2r5s2.42lisboa.com>
2022-07-26 20:31:19 +0100

    Update add_cmd_out
    Fix norminette plus open all output files

--- src/parser/get_cmd_list_utils.c
+++ src/parser/get_cmd_list_utils.c
@@ -6,7 +6,7 @@
 /*   By: nprimo <nprimo@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/06 16:01:37 by nprimo            #+#    #+#             */
-/*   Updated: 2022/07/22 21:27:57 by nprimo           ###   ########.fr       */
+/*   Updated: 2022/07/26 20:29:19 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,35 @@ int	add_cmd_in_out(t_cmd *cmd, t_list *cmd_token_list)
 	return (return_status);
 }
 
+static t_fd	update_fd_out(t_fd curr_out, t_token *curr_cont, t_list *curr_token)
+{
+	t_fd	out;
+	int		tmp_fd;
+	int		open_flags;
+
+	out.fname = NULL;
+	out.red = NULL;
+	if (curr_token->next)
+	{
+		if (curr_out.fname != NULL)
+		{
+			if (ft_strncmp(curr_out.red, ">", 2) == 0)
+				open_flags = O_WRONLY | O_CREAT | O_TRUNC;
+			else
+				open_flags = O_WRONLY | O_APPEND | O_CREAT;
+			tmp_fd = open(curr_out.fname, open_flags, 0622);
+			close(tmp_fd);
+		}
+		out.fname = ft_replace(curr_out.fname,
+				((t_token *) curr_token->next->content)->s);
+		out.red = ft_replace(curr_out.red,
+				curr_cont->s);
+		return (out);
+	}
+	out.fd = -256;
+	return (out);
+}
+
 static int	add_cmd_out(t_cmd *cmd, t_list *cmd_token_list)
 {
 	t_list	*curr_token;
@@ -44,14 +73,8 @@ static int	add_cmd_out(t_cmd *cmd, t_list *cmd_token_list)
 		else if ((ft_strncmp(">", curr_cont->s, 2) == 0
 				|| (ft_strncmp(">>", curr_cont->s, 3) == 0)))
 		{
-			if (curr_token->next)
-			{
-				cmd->out.fname = ft_replace(cmd->out.fname,
-						((t_token *) curr_token->next->content)->s);
-				cmd->out.red = ft_replace(cmd->out.red,
-						curr_cont->s);
-			}
-			else
+			cmd->out = update_fd_out(cmd->out, curr_cont, curr_token);
+			if (cmd->out.fd == -256)
 				return (258);
 		}
 		curr_token = curr_token->next;
