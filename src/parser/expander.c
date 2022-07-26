c82d357945883a70025253e2e1d3f9fe1dfbd04e
Gabriel Contarini <gcontarini@gmail.com>
2022-07-26 12:44:24 +0100

    Properly fix exit status free in expander

--- src/parser/expander.c
+++ src/parser/expander.c
@@ -6,7 +6,7 @@
 /*   By: gcontari <gcontari@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/21 09:24:17 by gcontari          #+#    #+#             */
-/*   Updated: 2022/07/26 12:39:30 by gcontari         ###   ########.fr       */
+/*   Updated: 2022/07/26 12:43:50 by gcontari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,7 +73,7 @@ static void	cpy_and_exp(char *dst, const char *src, t_exp exp, t_shell sh)
 			while (p && *p)
 				*dst++ = *p++;
 			if (*exp.vars && **exp.vars == '?')
-				free(p - 1);
+				free(p - ft_intlen(sh.exit_status));
 			exp.table++;
 			exp.vars++;
 			continue ;
